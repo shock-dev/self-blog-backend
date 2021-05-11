eef7bd7b37a425a04900edb4cc9dbc7544bf263d
Andrey <shock.work@mail.ru>
2021-05-11 19:32:19 +0300

    use posts routes in index.ts and more

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,19 @@
 import dotenv from 'dotenv';
-dotenv.config();
-
 import express from 'express';
+import cors from 'cors';
 import consola from 'consola';
 import connect from './core/db';
+import postsRoutes from './routes/posts';
+
+dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 5000;
+
+app.use(express.json());
+app.use(cors());
+
+app.use('/api/posts', postsRoutes);
 
 connect()
   .then(() => {

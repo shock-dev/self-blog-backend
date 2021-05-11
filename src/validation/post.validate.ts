dc0a16c59066bf11daf25a43a7f942b5edd9905f
Andrey <shock.work@mail.ru>
2021-05-11 21:31:09 +0300

    basic validation for posts

---
+++ src/validation/post.validate.ts
@@ -0,0 +1,16 @@
+import { body } from 'express-validator';
+
+const title = { min: 3, max: 70 };
+const description = { min: 10, max: 500 };
+
+export default [
+  body('title', 'Enter the title')
+    .isString()
+    .withMessage('Title must be a string')
+    .isLength(title)
+    .withMessage(`Allowed number of characters in title from ${title. min} to ${title. max}`),
+  body('description', 'Enter the description')
+    .isString()
+    .isLength(description)
+    .withMessage(`Allowed number of characters in description from ${description. min} to ${description. max}`)
+];

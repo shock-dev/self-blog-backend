b59307c212f1d2fcf054c46afc86bef4d053aab3
Andrey <shock.work@mail.ru>
2021-05-13 21:15:47 +0300

    realize registration [without validation]

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,11 +1,36 @@
 import { Request, Response } from 'express';
+import bcrypt from 'bcryptjs';
 import { ResBody } from '../types/response';
+import { IUser } from '../types/user';
+import User from '../models/User';
 
 class AuthController {
-  async register(req: Request, res: Response<ResBody>) {
+  async register(req: Request<{}, {}, IUser>, res: Response<ResBody>) {
     try {
+      const { email, username, password } = req.body;
+      const candidate = await User.findOne({ email });
+
+      if (candidate) {
+        console.log(candidate);
+
+        return res.status(500).json({
+          status: 'error',
+          message: 'User with this email already exists'
+        });
+      }
+
+      const salt = bcrypt.genSaltSync(10);
+      const hash = bcrypt.hashSync(password, salt);
+
+      const user = await User.create({
+        email,
+        username,
+        password: hash
+      });
+
       res.json({
-        status: 'ok'
+        status: 'ok',
+        data: user
       });
     } catch (e) {
       res.status(500).json({

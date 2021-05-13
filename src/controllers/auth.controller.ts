e7b6fa6a354582a8405aca99eed6936e0cedc31b
Andrey <shock.work@mail.ru>
2021-05-13 22:34:19 +0300

    check errors of validation in auth controller

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -3,16 +3,24 @@ import bcrypt from 'bcryptjs';
 import { ResBody } from '../types/response';
 import { IUser } from '../types/user';
 import User from '../models/User';
+import { validationResult } from 'express-validator';
 
 class AuthController {
   async register(req: Request<{}, {}, IUser>, res: Response<ResBody>) {
     try {
+      const errors = validationResult(req);
+
+      if (!errors.isEmpty()) {
+        return res.status(400).json({
+          status: 'error',
+          data: errors.array()
+        });
+      }
+
       const { email, username, password } = req.body;
       const candidate = await User.findOne({ email });
 
       if (candidate) {
-        console.log(candidate);
-
         return res.status(500).json({
           status: 'error',
           message: 'User with this email already exists'

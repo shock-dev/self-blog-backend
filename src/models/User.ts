ef2def424f41f4b16ad046c22386c9f11995379d
Andrey <shock.work@mail.ru>
2021-05-13 21:04:17 +0300

    make username not unique

--- src/models/User.ts
+++ src/models/User.ts
@@ -3,7 +3,7 @@ import { IUser } from '../types/user';
 
 const userSchema = new Schema({
   email: { type: String, required: true, unique: true },
-  username: { type: String, required: true, unique: true },
+  username: { type: String, required: true },
   password: { type: String, required: true, select: false }
 });
 

193ad5e0a01b6be9aac95b797b36e08f01b54c08
Andrey <shock.work@mail.ru>
2021-05-14 21:17:49 +0300

    login transfer to controller and add get user info

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,11 +1,10 @@
 import { Router } from 'express';
 import controller from '../controllers/auth.controller';
 import validate from '../validation/register.validate';
-import login from '../middlewares/login';
 
 const router = Router();
 
 router.post('/register', ...validate, controller.register);
-router.post('/login', login);
+router.post('/login', controller.login);
 
 export default router;

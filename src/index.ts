d4dab6ab2b2d36700f9364df805fe04f15eee294
Andrey <shock.work@mail.ru>
2021-05-14 17:46:52 +0300

    initialize passport

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import dotenv from 'dotenv';
 import express from 'express';
 import cors from 'cors';
+import passport from 'passport';
 import consola from 'consola';
 import connect from './core/db';
 
@@ -15,6 +16,7 @@ const port = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use(cors());
+app.use(passport.initialize());
 
 app.use('/api/posts', postsRoutes);
 app.use('/api/auth', authRoutes);

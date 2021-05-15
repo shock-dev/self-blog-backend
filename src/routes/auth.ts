eda8080f9aba1f182b51862a2e9bda74cd21a79e
Andrey <shock.work@mail.ru>
2021-05-15 13:08:17 +0300

    add middlewares to routes

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,10 +1,11 @@
 import { Router } from 'express';
 import controller from '../controllers/auth.controller';
 import validate from '../validation/register.validate';
+import login from '../middlewares/login';
 
 const router = Router();
 
 router.post('/register', ...validate, controller.register);
-router.post('/login', controller.login);
+router.post('/login', login);
 
 export default router;

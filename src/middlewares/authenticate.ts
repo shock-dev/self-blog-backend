393bcdeb8106ed5ef05476df2c443e3e113aabce
Andrey <shock.work@mail.ru>
2021-05-15 13:07:02 +0300

    add 2 middleware for auth and login

---
+++ src/middlewares/authenticate.ts
@@ -0,0 +1,20 @@
+import passport from 'passport';
+import { NextFunction, Request, Response } from 'express';
+
+const authenticate = (req: Request, res: Response, next: NextFunction) => {
+  return passport.authenticate('jwt', {
+    session: false
+  }, (err, user, info) => {
+    if (!user || !err) {
+      return res.json({
+        status: 'error',
+        error: info.message
+      });
+    }
+
+    req.user = user;
+    next();
+  })(req, res, next);
+};
+
+export default authenticate;

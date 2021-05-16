1f6150d71fea73ebf7fbbcfa1c6b2038ffa7d22b
Andrey <shock.work@mail.ru>
2021-05-16 20:12:35 +0300

    configure cors

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import express from 'express';
 import cors from 'cors';
 import passport from 'passport';
 import consola from 'consola';
+import cookieParser from 'cookie-parser';
 import connect from './core/db';
 
 import postsRoutes from './routes/posts';
@@ -15,7 +16,11 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+  credentials: true,
+  origin: 'http://localhost:3000'
+}));
+app.use(cookieParser());
 app.use(passport.initialize());
 
 app.use('/api/posts', postsRoutes);

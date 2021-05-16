f8f9c527c2688a5fd60eedd043c96e61df158833
Andrey <shock.work@mail.ru>
2021-05-16 22:45:40 +0300

    install forgotten `cookieParser`

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import express from 'express';
 import cors from 'cors';
 import passport from 'passport';
 import consola from 'consola';
+import cookieParser from 'cookie-parser';
 import connect from './core/db';
 
 import postsRoutes from './routes/posts';
@@ -15,6 +16,7 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(express.json());
+app.use(cookieParser());
 app.use(cors({
   credentials: true,
   origin: 'http://localhost:3000'

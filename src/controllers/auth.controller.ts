33ac5daa671bf6ee587219236691681946a50bed
Andrey <shock.work@mail.ru>
2021-05-17 12:04:01 +0300

    realize login in auth controller

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,9 +1,9 @@
 import { Request, Response } from 'express';
 import bcrypt from 'bcryptjs';
+import { validationResult } from 'express-validator';
 import { ResBody } from '../types/response';
 import { IUser } from '../types/user';
 import User from '../models/User';
-import { validationResult } from 'express-validator';
 import generateJWT from '../utils/generateJWT';
 
 class AuthController {
@@ -58,7 +58,54 @@ class AuthController {
     } catch (e) {
       res.status(500).json({
         status: 'error',
-        data: e
+        data: e.message
+      });
+    }
+  }
+
+  async login(req: Request, res: Response<ResBody>) {
+    try {
+      const { email, password } = req.body;
+      const user: IUser | null = await User.findOne({ email }).select('+password');
+
+      if (!user) {
+        return res.status(400).json({
+          status: 'error',
+          data: 'invalid username or password'
+        });
+      }
+
+      const isValidPassword = bcrypt.compareSync(password, user.password);
+
+      if (!isValidPassword) {
+        return res.status(400).json({
+          status: 'error',
+          data: 'invalid username or password'
+        });
+      }
+
+      const token = generateJWT(user);
+
+      res.cookie(
+        'authToken',
+        token,
+        { maxAge: 30 * 24 * 60 * 60 * 1000 }
+      );
+
+      const data = {
+        id: user._id,
+        username: user.username,
+        email: user.email
+      };
+
+      res.json({
+        status: 'ok',
+        data
+      });
+    } catch (e) {
+      res.status(500).json({
+        status: 'error',
+        data: e.message
       });
     }
   }

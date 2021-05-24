b3a93b92f13e255f4adba4bb35634822baf39757
Andrey <shock.work@mail.ru>
2021-05-24 22:12:45 +0300

    add upload and jwt in creating route

--- src/routes/posts.ts
+++ src/routes/posts.ts
@@ -1,12 +1,20 @@
 import { Router } from 'express';
 import controller from '../controllers/posts.controller';
 import validate from '../validation/post.validate';
+import upload from '../core/upload';
+import checkJWT from '../middlewares/checkJWT';
 
 const router = Router();
 
 router.get('/', controller.all);
 router.get('/:id', controller.one);
-router.post('/', ...validate, controller.create);
+router.post(
+  '/',
+  upload.single('intro'),
+  checkJWT(),
+  ...validate,
+  controller.create
+);
 router.put('/:id', ...validate, controller.update);
 router.delete('/:id', controller.destroy);
 

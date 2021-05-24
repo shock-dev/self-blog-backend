969f5fe52cc1bee380dbdf1fec17e3ef9b3bb6f3
Andrey <shock.work@mail.ru>
2021-05-24 22:09:16 +0300

    update types and model of post

--- src/types/post.ts
+++ src/types/post.ts
@@ -3,6 +3,8 @@ import { Document } from 'mongoose';
 export interface IPost extends Document {
   title: string
   description: string,
+  user: String
   imageUrl?: String
   views?: number
+  cloudinaryId?: string
 }

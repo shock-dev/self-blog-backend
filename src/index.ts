9d9ecc02e1da8eda3cc444b371dc2625dab1c339
Andrey <shock.work@mail.ru>
2021-06-10 14:09:06 +0300

    set optionsSuccessStatus in cors

--- src/index.ts
+++ src/index.ts
@@ -19,7 +19,8 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
   credentials: true,
-  origin: 'http://localhost:3000'
+  origin: 'http://localhost:3000',
+  optionsSuccessStatus: 200
 }));
 app.use(passport.initialize());
 

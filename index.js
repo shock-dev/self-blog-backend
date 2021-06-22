6b95c85606d0d3f97b41fba95faf97d889bc51f6
Andrey <shock.work@mail.ru>
2021-06-22 17:12:37 +0300

    connect to db in index.js

--- index.js
+++ index.js
@@ -4,12 +4,16 @@ const cors = require('cors');
 const passport = require('passport');
 const consola = require('consola');
 const cookieParser = require('cookie-parser');
-const connect = require('./src/core/db');
+const mongoose = require('mongoose');
 
 dotenv.config();
 
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT;
+
+if (!port || typeof +port !== 'number') {
+  throw new Error('Для запуска необходим порт');
+}
 
 app.use(express.json());
 app.use(cookieParser());
@@ -24,9 +28,21 @@ app.use('/api/posts', require('./src/routes/posts'));
 app.use('/api/auth', require('./src/routes/auth'));
 app.use('/api/users', require('./src/routes/users'));
 
-connect()
-  .then(() => {
-    app.listen(port, () => {
-      consola.success(`Server has been started at ${port} port`);
+const start = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URL, {
+      useNewUrlParser: true,
+      useCreateIndex: true,
+      useUnifiedTopology: true,
+      useFindAndModify: false
     });
-  });
+    consola.success('Database connected');
+    app.listen(port, () => {
+      consola.success(`Server started on http://localhost:${port}`);
+    });
+  } catch (error) {
+    consola.error(error);
+  }
+};
+
+start();

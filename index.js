202405d8985935de466da01f3d76a912d1b8754e
Andrey <shock.work@mail.ru>
2021-06-24 19:19:05 +0300

    set origin for dev

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.use(cookieParser());
 app.use(passport.initialize());
 app.use(cors({
   credentials: true,
-  origin: 'https://self-blog.vercel.app',
+  origin: 'http://localhost:3000',
   optionsSuccessStatus: 200
 }));
 
